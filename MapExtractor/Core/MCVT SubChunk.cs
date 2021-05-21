5c10e0252b228b1c567f8b5c55284a5b87d83b6c
devwar87 <72774832+devwar87@users.noreply.github.com>
2021-05-20 21:34:57 -0500

    Fill MCVT and MCNR vertices interleaved. (9,8,9,8)

--- MapExtractor/Core/MCVT SubChunk.cs
+++ MapExtractor/Core/MCVT SubChunk.cs
@@ -2,22 +2,37 @@
 // Discord: https://discord.gg/RzBMAKU
 // Github:  https://github.com/The-Alpha-Project
 
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace AlphaCoreExtractor.Core
 {
     /// It's composed of the usual 145 floats, but their order is different : alpha vertices are not interleaved... 
-    /// Which means there are all outer vertices first (all 81), then all inner vertices (all 64) in MCVT (and not 9-8-9-8 etc.). 
+    /// Which means there are all outer vertices first (all 81), then all inner vertices (all 64) in MCVT (and not 9-8-9-8). 
     /// Unlike 3.x format, MCVT have absolute height values (no height relative to MCNK header 0x70).
     /// </summary>
     public class MCVTSubChunk
     {
-        float[] Heights = new float[9 * 9 + 8 * 8];
+        public List<float> Heights = new List<float>(); // [9 * 9 + 8 * 8] (145);
 
         public MCVTSubChunk(BinaryReader reader)
         {
-            for (int i = 0; i < Heights.Length; i++)
-                Heights[i] = reader.ReadSingle();
+            // Interleave vertices (9-8-9-8)
+            using (BinaryReader outerVerticesReader = new BinaryReader(new MemoryStream(reader.ReadBytes(324)))) // 81 * 4bytes
+            using (BinaryReader innerVerticesReader = new BinaryReader(new MemoryStream(reader.ReadBytes(256)))) // 64 * 4bytes
+            {
+                while(outerVerticesReader.BaseStream.Position != outerVerticesReader.BaseStream.Length)
+                {
+                    for (int i = 0; i < 9; i++)
+                        Heights.Add(outerVerticesReader.ReadSingle());
+
+                    // If we reached the end, skip inner vertices.
+                    if (innerVerticesReader.BaseStream.Position != innerVerticesReader.BaseStream.Length)
+                        for (int j = 0; j < 8; j++)
+                            Heights.Add(innerVerticesReader.ReadSingle());
+                }
+            }
         }
     }
 }
