5dec71dcf4af28c315d38f18cf6516c13e23d498
devwar87 <72774832+devwar87@users.noreply.github.com>
2021-08-07 14:59:28 -0500

    Update CMapObj.cs

--- MapExtractor/Core/CMapObj.cs
+++ MapExtractor/Core/CMapObj.cs
@@ -70,7 +70,7 @@ namespace AlphaCoreExtractor.Core
         /// <summary>
         /// How many tiles we have information for.
         /// </summary>
-        public uint UnUsableTiles => (64 * 64) - UsableTiles;
+        public uint UnUsableTiles => ((uint)Constants.TileBlockSize * (uint)Constants.TileBlockSize) - UsableTiles;
 
         /// <summary>
         /// Used to read file tokens and validate chunks.
